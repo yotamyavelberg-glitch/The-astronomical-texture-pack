b7d39d880196c0be8a78f8ae4a37b135d62f71c7
yotamyavelberg-glitch <yotamyavelberg@gmail.com>
2026-03-20 21:18:41 +0200

    Add files via upload

---
+++ The astronomical texture pack/main.lua
@@ -0,0 +1,21 @@
+
+
+AltTexture({
+  key = 'Astronomical',
+  set = 'Joker', 
+  path = 'jokas.png',
+  original_sheet = true,
+  loc_txt = { 
+    name = 'The astronomical texture pack',
+  }
+})
+
+TexturePack({
+key = 'astronomicalistic',
+textures = {'Astro_Astronomical'},
+loc_txt = {
+name = 'The astronomical texture pack',
+text = {' changes some of the joker textures in the game!'}
+}
+}
+)
